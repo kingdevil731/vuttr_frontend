2bd58056b0d66630a8ef7b577b5f8209e2900ca9
ruben forner <9657174+kingdevil731@users.noreply.github.com>
2020-08-29 17:17:33 +0200

    2

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,28 +1,28 @@
 import React from 'react';
 //styles
 import './styles.css';
+//
+import logo from '../../Assets/logo512.png';
 
 const Main = () => {
 
 
     return (
       <div className="main-interface">
+        <section className="main-interface-text">
+          <img src={logo} alt="logo da aplicacao" />
 
           <h1>Bem vindo ao Vuttr</h1>
 
           <h2>
-            Uma aplicacao onde temos ferramentas que sao muito uteis de se lembrar
+            Uma aplicacao onde temos ferramentas que sao muito uteis de se
+            lembrar
           </h2>
-
-          <h3>
-              Para poder prosseguir efectue a autenticacao do usuario
-          </h3>
-
-          <div className="selecao">
-              <button>Iniciar seccao</button>
-              <button>Cadastar</button>
-          </div>
- 
+        </section>
+        <div className="selecao">
+          <button className="button-log">Iniciar seccao</button>
+          <button className="button-log">Cadastar</button>
+        </div>
       </div>
     );
 }
